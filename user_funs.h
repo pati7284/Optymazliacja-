a6ef3da88bf4a3da58c973ed03f227bdae04cc81
pati7284 <patrycja.mazur1@hotmail.com>
2025-10-18 12:59:48 +0200

    add: dodano funkcje df1

--- user_funs.h
+++ user_funs.h
@@ -7,4 +7,5 @@ matrix ff0R(matrix, matrix = NAN, matrix = NAN);
 matrix df0(double, matrix, matrix = NAN, matrix = NAN);
 
 matrix ff1L(matrix x, matrix ud1 = NAN, matrix ud2 = NAN);
+matrix df1(double t, matrix Y, matrix ud1, matrix ud2);
 
